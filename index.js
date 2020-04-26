d26e72eabbc7b42d5a74cad6226950630d96eb47
romilrh <romilrh@gmail.com>
2020-04-25 19:08:38 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const CORRECTTEXT = "Correct!";
+const INCORRECTTEXT = "Not quite!";
+
+let score = 0;
+let points = 0;
+
+$('.button').click((e) => {
+    // Get states
+    let target = $(e.target)
+    let num = target.attr("number");
+
+    // Set feedback text
+    $(`p[number=${num}]`).text(target.hasClass('correct') ? CORRECTTEXT : INCORRECTTEXT);
+
+    // Show text
+    $(`div[number=${num}]`).toggleClass("hidden");
+
+    // Style buttons
+    $(`button[number=${num}]`).not(".correct").toggleClass("is-danger");
+    $(`button[number=${num}].correct`).toggleClass("is-primary");
+    $(`button[number=${num}]`).attr("disabled", 1);
+});
